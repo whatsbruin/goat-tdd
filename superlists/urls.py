2d9b0efbcfdd7206fa9d6bcabb79cdee127875e5
jrdexter <c-james.dexter@charter.com>
2018-11-12 16:46:48 -0700

    fcuking finally- good lord with the _from lists import views_

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import re_path, include
+from lists import views
+urlpatterns = [ 
+    #path('admin/', admin.site.urls),
+    re_path(r'^$', views.home_page),
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
 ]
