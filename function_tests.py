2fe45bb8c8e9cf53cfe0f3992078aa5474074dcd
jrdexter <c-james.dexter@charter.com>
2018-11-12 17:19:56 -0700

    Basic view with tests adn mini-minimal html

--- function_tests.py
+++ function_tests.py
@@ -12,8 +12,8 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrive_it_later(self):
         self.browser.get('http://localhost:8000')
 
-        self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        self.assertIn('To-Do Lists', self.browser.title)
+        #self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
