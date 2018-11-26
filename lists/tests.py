e8eb0514920fa990345151b363f92435a140e9f9
jrdexter <c-james.dexter@charter.com>
2018-11-26 14:20:19 -0700

    refactored home page view with a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,10 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
+
 
 class HomePageTest(TestCase):
     
@@ -13,8 +15,6 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do Lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
-
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
 
